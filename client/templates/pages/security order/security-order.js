58f0a61e69de58a17f23b9fb916199c68fd6a5f2
Steven Scaffidi <sscaff1@gmail.com>
2015-11-24 00:01:27 -0800

    added security order form

--- client/templates/pages/security order/security-order.js
+++ client/templates/pages/security order/security-order.js
@@ -1,38 +1,80 @@
 Template.securityOrder.onCreated(function() {
-  Session.set('loopWorkTimes', [{
+  var instance = this;
+  instance.totalPrice = new ReactiveVar(0);
+  instance.totalArmed = new ReactiveVar(0);
+  instance.totalUnarmed = new ReactiveVar(0);
+  instance.incentiveDate = new ReactiveVar(null);
+  instance.paymentMethod = new ReactiveVar(false);
+  instance.loopWorkTimes = new ReactiveVar([{
     personnel: '',
     booth: '',
     startTime: '',
     endTime: ''
   }]);
-  Session.set('totalPrice', 0);
   Session.set('postSubmitErrors', {});
-  Session.set('dateMinus21', null);
 });
 
 Template.securityOrder.onRendered(function() {
-  this.$('.datetimepicker').datetimepicker();
-  this.$('.datepicker').datetimepicker({
+  var instance = this;
+  instance.$('.datetimepicker').datetimepicker({
+    sideBySide: true,
+    showClose: true
+  });
+
+  instance.$('.datepicker').datetimepicker({
     format: 'MM/DD/YYYY'
   });
+  instance.totalPrice.set(0);
+  instance.totalArmed.set(0);
+  instance.totalUnarmed.set(0);
+  instance.incentiveDate.set(null);
+  instance.loopWorkTimes.set([{
+    personnel: '',
+    booth: '',
+    startTime: '',
+    endTime: ''
+  }]);
 });
 
 Template.securityOrder.helpers({
   loopWorkTimes: function() {
-    return Session.get('loopWorkTimes');
+    return Template.instance().loopWorkTimes.get();
   },
   totalPrice: function() {
-    return Session.get('totalPrice');
+    return Template.instance().totalPrice.get();
   },
   dateMinus21: function() {
-    return Session.get('dateMinus21');
+    return Template.instance().incentiveDate.get();
+  },
+  totalArmed: function() {
+    return Template.instance().totalArmed.get();
+  },
+  totalUnarmed: function() {
+    return Template.instance().totalUnarmed.get();
+  },
+  noncredit: function() {
+    var paymentMethod = Template.instance().paymentMethod.get();
+    if (paymentMethod === 'credit') {
+      return {
+        selected: true,
+        noncredit: false
+      }
+    } else if (paymentMethod === 'noncredit'){
+      return {
+        selected: true,
+        noncredit: true
+      }
+    } else {
+      return {
+        selected: false
+      }
+    }
   }
 });
 
 Template.securityOrder.events({
-  'click #moreWorkTimes, blur form': function(event, template) {
+  'click #moreWorkTimes, blur .form-control': function(event, template) {
     event.preventDefault();
-
     var currentWorkTimes = [];
     var workTimes = template.findAll('[name=workTime]');
     _.each(workTimes, function(workTime) {
@@ -48,17 +90,17 @@ Template.securityOrder.events({
     if ($(event.target).attr('id') === 'moreWorkTimes') {
       currentWorkTimes.push({});
     }
-    Session.set('loopWorkTimes', currentWorkTimes);
+    template.loopWorkTimes.set(currentWorkTimes);
     Meteor.setTimeout(function() {
       template.$('.datetimepicker').datetimepicker();
     }, 250);
   },
-  'click form, blur form': function(event, template) {
+  'blur .form-control': function(event, template) {
     event.preventDefault();
-    var currentWorkTimes = Session.get('loopWorkTimes');
+
+    var currentWorkTimes = template.loopWorkTimes.get();
     if (currentWorkTimes.length > 0) {
       var eventDate = moment(template.$('[name=eventDate]').val(),'MM/DD/YYYY');
-      Session.set('dateMinus21', eventDate.subtract(21, 'days').format('MM/DD/YYYY'));
       var today = moment().startOf('day');
       var daysToEvent = Math.round(moment.duration(eventDate-today).asDays());
       var armed; var unarmed;
@@ -70,18 +112,35 @@ Template.securityOrder.events({
         unarmed = 30;
       }
       var totalPrice = 0;
+      var totalArmed = 0;
+      var totalUnarmed = 0;
       _.each(currentWorkTimes, function(workTime) {
         var startTime = moment(workTime.startTime, 'MM-DD-YYYY H:mm A');
         var endTime = moment(workTime.endTime, 'MM-DD-YYYY H:mm A');
         var totalTime = endTime.diff(startTime, 'hours');
-        if (workTime.type === 'armed') {
-          totalPrice = armed * workTime.personnel * totalTime
-        } else {
-          totalPrice = unarmed * workTime.personnel * totalTime
+        if (totalTime > 0) {
+          if (workTime.type === 'armed') {
+            totalArmed = totalArmed + totalTime*workTime.personnel;
+            totalPrice = totalPrice + armed * workTime.personnel * totalTime;
+          } else {
+            totalUnarmed = totalUnarmed + totalTime*workTime.personnel;
+            totalPrice = totalPrice + unarmed * workTime.personnel * totalTime;
+          }
         }
       });
-      Session.set('totalPrice', totalPrice);
+      if (totalPrice > 0) {
+        template.totalArmed.set(totalArmed);
+        template.totalUnarmed.set(totalUnarmed);
+        template.totalPrice.set(totalPrice);
+      }
     }
+  },
+  'blur #eventDate': function(event, template) {
+    var eventDate = moment(template.$('[name=eventDate]').val(),'MM/DD/YYYY');
+    template.incentiveDate.set(eventDate.subtract(21, 'days').format('MM/DD/YYYY'));
+  },
+  'change [name=paymentMethod]': function(event,template) {
+    template.paymentMethod.set(template.$(event.target).val());
   }
 });
 
@@ -89,12 +148,5 @@ Template.workTimes.events({
   'blur .startTime': function(event, template) {
     var startTime = template.$('[name=startTime]').val();
     template.$('[name=endTime]').val(startTime);
-  },
-  'blur .endTime': function(event, template) {
-    var startTime = moment(template.$('[name=startTime]').val(), 'MM-DD-YYYY H:mm A');
-    var endTime = moment(template.$('[name=endTime]').val(), 'MM-DD-YYYY H:mm A');
-    var totalTime = endTime.diff(startTime, 'hours');
-    var totalPersonnel = template.$('[name=personnel]').val()
-    template.$('[name=totalTime]').text(totalTime*totalPersonnel);
   }
 });
